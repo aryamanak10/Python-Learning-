dd9c7844136b2c4d5a1082b2f3c2cdaf8b2b9589
Aryaman Kakad <askvit2000@gmail.com>
2020-04-18 00:43:08 +0530

    Second Commit from Day 1

--- Day 1/main.py
+++ Day 1/main.py
@@ -1,2 +1,12 @@
 def my_print(txt):
     print(txt)
+
+msg_template  = """Hello {name},
+Thank you for joining {website}.We are very
+happy to have you with us.
+"""
+
+def format_msg(my_name="Aryaman",my_website="github.com"):
+    my_msg = msg_template.format(name=my_name, website=my_website)
+    # print(my_msg)
+    return my_msg
