2f3e921d2dad04d6675e8ae0b709bc8a4287c224
Aryaman Kakad <askvit2000@gmail.com>
2020-04-18 11:14:11 +0530

    Third Commit for Day 2

--- Day 1/main.py
+++ Day 1/main.py
@@ -10,3 +10,12 @@ def format_msg(my_name="Aryaman",my_website="github.com"):
     my_msg = msg_template.format(name=my_name, website=my_website)
     # print(my_msg)
     return my_msg
+
+"""
+"{} {}".format("abc",123)
+"{1} {0}".format("abc",123)
+"{name} {number}".format(name="abc",number=123)
+"{} {name} {number}".format("another",name="abc",number=123)
+
+def fucntion(*args, **kwargs):
+    print(args, kwargs)
