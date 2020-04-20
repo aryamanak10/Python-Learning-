3851d9e887c29b415dfaf8f6ae93d2488e6ec467
Aryaman Kakad <askvit2000@gmail.com>
2020-04-20 23:37:35 +0530

    Day_3_Send_Email Commit 2

--- Day 3/send_mail.py
+++ Day 3/send_mail.py
@@ -1,10 +1,23 @@
 import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 
 username = 'akakad671@gmail.com'
 password = 'aryamanisgr8@'
 
-def send_mail(text='Email Body', subject='Hello World', from_email='akakad671@gmail.com', to_emails=None):
+def send_mail(text='Email Body', subject='Hello World', from_email='Lionel Messi <akakad671@gmail.com>', to_emails=None):
     assert isinstance(to_emails, list)
+    msg = MIMEMultipart('alternative')
+    msg['From'] = from_email
+    msg['To'] = ", ".join(to_emails)
+    msg['Subject'] = subject
+
+    txt_part = MIMEText(text, 'plain')
+    msg.attach(txt_part)
+
+    html_part = MIMEText("<h1> This is working </h1>", 'html')
+    #msg.attach(html_part)
+    msg_str = msg.as_string()
 
     # login in to my smtp server
     server = smtplib.SMTP(host='smtp.gmail.com', port = 547)
